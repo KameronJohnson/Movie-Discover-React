8d809e19fce8cbe03a5a418ad3105a4bad71b755
kameron johnson <kameronjohnson@gmail.com>
2018-03-13 20:39:48 -0700

    add Route params, display movie poster, image, title and overview on MovieDetail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
 import MoviesList from './Components/MoviesList';
-
-const Test = () => (
-  <h2>Test</h2>
-);
+import MovieDetail from './Components/MovieDetail';
 
 const App = () => (
   <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
         <h1 className="App-title">Welcome to React</h1>
       </header>
       <Switch>
         <Route exact path="/" component={MoviesList} />
-        <Route path="/test" component={Test} />
+        <Route path="/:id" component={MovieDetail} />
       </Switch>
     </div>
   </Router>
